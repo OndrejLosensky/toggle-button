899bbf032f0517b65522e9ae0692054263c092a0
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-08 16:01:36 +0200

    přidání Toggle button komponenty

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Header from "./components/header/Header";
+import Toggle from "./components/toggle/Toggle";
+
 
 function App() {
   return (
     <section className="w-full h-screen bg-black">
         <div>
          <Header/>
+        </div>
+        <div>
+         <Toggle/>
         </div>
     </section>
    
---
+++ src/components/toggle/Toggle.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Toggle = () => {
+
+    
+    return(
+        <div>
+            <label htmlFor="toggle-button">
+                <input type="checkbox" id="toggle-button" className="cursor-pointer h-32 w-64 rounded-full appearance-none bg-slate-50"/>
+            </label>
+        </div>
+    )
+}
+
+export default Toggle
