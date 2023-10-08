4f9eaddf084cd025fa2486136967cae43f765bec
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-08 16:29:15 +0200

    změna barev

--- src/components/toggle/Toggle.js
+++ src/components/toggle/Toggle.js
@@ -8,7 +8,7 @@ const Toggle = () => {
         <div>
             <label htmlFor="toggle-button">
                 <input type="checkbox" id="toggle-button" className="cursor-pointer h-32 w-64 rounded-full appearance-none 
-                bg-slate-50 bg-opacity-5 border-textColor border-3 checked:bg-slate-400 transition duration-300 relative"/>
+                bg-borderColor bg-opacity-5 border-textColor border-3 checked:bg-foreColor transition duration-300 relative"/>
             </label>
         </div>
     )
