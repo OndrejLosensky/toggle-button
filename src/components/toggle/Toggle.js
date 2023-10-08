1af869e332d009aea903d69a998767b32eb5a5ab
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-08 17:04:01 +0200

    přidání emoji do tlačítka

--- src/components/toggle/Toggle.js
+++ src/components/toggle/Toggle.js
@@ -1,17 +1,42 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 const Toggle = () => {
+    const [isChecked, setIsChecked] = useState(false);
+    const [emoji, setEmoji] = useState("☀️"); 
+    const [isEmojiVisible, setIsEmojiVisible] = useState(true);
 
-    
-    return(
-        <div>
+    const toggleHandler = () => {
+        setIsChecked(!isChecked);
+        setIsEmojiVisible(false); 
+
+       
+        setTimeout(() => {
+            setEmoji(isChecked ? "☀️":"🌙");
+            setIsEmojiVisible(true); 
+        }, 100); 
+    };
+
+    return (
+        <div className="relative">
             <label htmlFor="toggle-button">
-                <input type="checkbox" id="toggle-button" className="cursor-pointer shadow-xl h-24 w-60 rounded-full appearance-none 
-                bg-whenNotChecked bg-opacity-75 border-textColor border-3 checked:bg-whenChecked transition duration-300 relative"/>
+                <input
+                    type="checkbox"
+                    id="toggle-button"
+                    className="cursor-pointer shadow-xl h-24 w-60 rounded-full appearance-none bg-whenNotChecked bg-opacity-75 border-textColor border-3 checked:bg-whenChecked transition duration-300 relative"
+                    onChange={toggleHandler}
+                    checked={isChecked}
+                />
             </label>
+            <div
+                className={`absolute top-0 bottom-0 ${
+                    isChecked ? "left-8" : "right-8"
+                } flex items-center`}
+                style={{ fontSize: "32px", visibility: isEmojiVisible ? "visible" : "hidden" }}
+            >
+                {emoji}
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Toggle
+export default Toggle;
