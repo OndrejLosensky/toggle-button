a20ef9fc4276f074a8406f822772075821e079eb
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-08 17:24:37 +0200

    přidání footeru pro copyright

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import Header from "./components/header/Header";
 import Toggle from "./components/toggle/Toggle";
-
+import Footer from "./components/footer/Footer"
 
 function App() {
   return (
     <section className="w-full h-screen bg-slate-400">
         <div>
           <Header/>
+          <p className="flex justify-center pt-4 text-slate-700 text-center">  
+            This is simple toggle button that you can re-use <br/>
+            You can just add it to your code by copying "toggle" folder <br/>
+            Or it's gonna be available on NPMjs.com as a package.
+          </p>
         </div>
         <div className="flex justify-center items-center h-[80%]">
           <Toggle/>
+        </div>
+        <div>
+           <Footer/>
         </div>
     </section>
    
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="w-full h-[80px] flex justify-center text-center absolute bottom-0 ">
+            <h1 className="uppercase text-xl font-thin py-8 text-white"> © 2023 - Losenský Ondřej </h1>
+        </div>
+    )
+}
+
+export default Footer
