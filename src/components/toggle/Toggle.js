04b6ef7929dfdb85b7c1bf78482c1089f93e67b4
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-08 16:15:11 +0200

    přidání RELATIVE

--- src/components/toggle/Toggle.js
+++ src/components/toggle/Toggle.js
@@ -8,7 +8,7 @@ const Toggle = () => {
         <div>
             <label htmlFor="toggle-button">
                 <input type="checkbox" id="toggle-button" className="cursor-pointer h-32 w-64 rounded-full appearance-none 
-                bg-slate-50 bg-opacity-5 border-textColor border-2 checked:bg-slate-400 transition duration-200"/>
+                bg-slate-50 bg-opacity-5 border-textColor border-2 checked:bg-slate-400 transition duration-200 relative"/>
             </label>
         </div>
     )
