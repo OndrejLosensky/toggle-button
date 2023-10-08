0ab593e0411be42f9d45af57a0f6f5d929dd4107
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-08 17:18:20 +0200

    úprava nadpisu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
     return (
         <div className="w-full h-[80px] flex justify-center text-center border-b-white border-b-[2px]">
-            <h1 className="uppercase text-4xl font-bold pt-6 text-white"> Toggle button </h1>
+            <h1 className="uppercase text-4xl font-bold pt-6 text-white"> Toggle-button </h1>
         </div>
     )
 }
