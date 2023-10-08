e7c508b28647154b4640d304b590f181e3f51579
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-08 16:11:41 +0200

    přidání dalších stylů

--- src/components/toggle/Toggle.js
+++ src/components/toggle/Toggle.js
@@ -7,7 +7,8 @@ const Toggle = () => {
     return(
         <div>
             <label htmlFor="toggle-button">
-                <input type="checkbox" id="toggle-button" className="cursor-pointer h-32 w-64 rounded-full appearance-none bg-slate-50 bg-opacity-5"/>
+                <input type="checkbox" id="toggle-button" className="cursor-pointer h-32 w-64 rounded-full appearance-none 
+                bg-slate-50 bg-opacity-5 border-textColor border-2 checked:bg-slate-400 transition duration-200"/>
             </label>
         </div>
     )
